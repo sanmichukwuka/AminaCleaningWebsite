6b2b61e0d619705fba4abbe77dacdbb95886a94b
sanmichukwuka <michaelsanmi09@gmail.com>
2020-07-19 16:40:26 -0500

    updated repository package

--- src/main/java/com/tts/acleaningwebsite/repository/ACleaningWebsiteRepository.java
+++ src/main/java/com/tts/acleaningwebsite/repository/ACleaningWebsiteRepository.java
@@ -1,14 +1,13 @@
 package com.tts.acleaningwebsite.repository;
 
-
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tts.acleaningwebsite.model.Scheduler;
 
-
 @Repository
 public interface ACleaningWebsiteRepository extends CrudRepository<Scheduler, Long>{
-
+	public Optional<Scheduler> findById(Long SchedulerId);
 }
